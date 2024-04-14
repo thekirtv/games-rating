b5016fad326a06fcade60caaf2b835008835337a
thekirtv <149680777+thekirtv@users.noreply.github.com>
2024-04-14 20:32:08 +0300

    Add files via upload

---
+++ WebKids-m4-v2-hw-pre-main-2/controllers/index.js
@@ -0,0 +1,9 @@
+const mainRouteController = require('./main');
+const gameRouteController = require('./game');
+const defaultRouteController = require('./default');
+
+module.exports = {
+    mainRouteController,
+    gameRouteController,
+    defaultRouteController
+};
