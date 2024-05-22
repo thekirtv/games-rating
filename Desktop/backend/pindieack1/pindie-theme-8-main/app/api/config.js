4f6b49860ad3b67756a0e7aa8f83dbcf0545e375
Zoka <lapoknyshka@mail.ru>
2024-05-22 13:59:14 +0300

    <сообщение коммита>

---
+++ Desktop/backend/pindieack1/pindie-theme-8-main/app/api/config.js
@@ -0,0 +1,7 @@
+export const BASE_URL = 'https://pirdie.nomoredomainswork.ru'
+
+export const endpoints = {
+  games: `${BASE_URL}/games`,
+  auth: `${BASE_URL}/auth/login`,
+  me: `${BASE_URL}/me`,
+}
